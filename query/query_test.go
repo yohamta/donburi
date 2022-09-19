22215398d922e91f80808312a76de46bc1b719e0
yohamta <yohamta@gmail.com>
2022-09-19 20:19:58 +0900

    refactor tests

--- query/query_test.go
+++ query/query_test.go
@@ -8,81 +8,72 @@ import (
 	"github.com/yohamta/donburi/query"
 )
 
-var PlayerTag = donburi.NewTag()
-var SecondaryTag = donburi.NewTag()
-var EnemyTag = donburi.NewTag()
+var (
+	tagA = donburi.NewTag()
+	tagB = donburi.NewTag()
+	tagC = donburi.NewTag()
+)
 
 func TestQuery(t *testing.T) {
-	nm := "TestQuery"
-
 	world := donburi.NewWorld()
-	world.Create(PlayerTag)
-	world.Create(EnemyTag)
-	world.Create(PlayerTag, SecondaryTag)
+	world.Create(tagA)
+	world.Create(tagC)
+	world.Create(tagA, tagB)
 
-	query := query.NewQuery(filter.Contains(PlayerTag))
+	query := query.NewQuery(filter.Contains(tagA))
 	count := 0
 	query.EachEntity(world, func(entry *donburi.Entry) {
 		count++
-		if entry.Archetype().Layout().HasComponent(PlayerTag) == false {
-			t.Errorf("%s: PlayerTag should be in player archetype", nm)
+		if entry.Archetype().Layout().HasComponent(tagA) == false {
+			t.Errorf("PlayerTag should be in ent archetype")
 		}
 	})
 
 	if count != 2 {
-		t.Errorf("%s: counter should be 2, but got %d", nm, count)
+		t.Errorf("counter should be 2, but got %d", count)
 	}
 }
 
 func TestQueryMultipleComponent(t *testing.T) {
-	nm := "TestQueryEntities"
-
 	world := donburi.NewWorld()
-	world.Create(PlayerTag)
-	world.Create(EnemyTag)
-	world.Create(PlayerTag, SecondaryTag)
 
-	query := query.NewQuery(filter.Contains(PlayerTag, SecondaryTag))
+	world.Create(tagA)
+	world.Create(tagC)
+	world.Create(tagA, tagB)
 
-	count := 0
-	query.EachEntity(world, func(_ *donburi.Entry) {
-		count++
-	})
-
+	query := query.NewQuery(filter.Contains(tagA, tagB))
+	count := query.Count(world)
 	if count != 1 {
-		t.Errorf("%s: counter should be 1, but got %d", nm, count)
+		t.Errorf("counter should be 1, but got %d", count)
 	}
 }
 
 func TestComplexQuery(t *testing.T) {
+	createWorldFunc := func() donburi.World {
+		world := donburi.NewWorld()
+
+		world.Create(tagA)
+		world.Create(tagC)
+		world.Create(tagA, tagB)
+
+		return world
+	}
+
 	var tests = []struct {
-		name   string
-		filter filter.LayoutFilter
-		want   int
+		filter        filter.LayoutFilter
+		expectedCount int
 	}{
-		{"Not filter", filter.Not(filter.Contains(PlayerTag)), 1},
-		{"And filter", filter.And(filter.Contains(PlayerTag), filter.Not(filter.Contains(SecondaryTag))), 1},
-		{"Or filter", filter.Or(filter.Contains(PlayerTag), filter.Contains(EnemyTag)), 3},
+		{filter.Not(filter.Contains(tagA)), 1},
+		{filter.And(filter.Contains(tagA), filter.Not(filter.Contains(tagB))), 1},
+		{filter.Or(filter.Contains(tagA), filter.Contains(tagC)), 3},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			world := donburi.NewWorld()
-			world.Create(PlayerTag)
-			world.Create(EnemyTag)
-			world.Create(PlayerTag, SecondaryTag)
-
-			query := query.NewQuery(tt.filter)
-			count := 0
-			query.EachEntity(world, func(entry *donburi.Entry) {
-				count++
-			})
-
-			if count != tt.want {
-				t.Errorf("%s: counter should be %d, but got %d", tt.name, tt.want, count)
-			}
-
-		})
+		world := createWorldFunc()
+		query := query.NewQuery(tt.filter)
+		count := query.Count(world)
+		if count != tt.expectedCount {
+			t.Errorf("counter should be %d, but got %d", tt.expectedCount, count)
+		}
 	}
-
 }
