5a0940ceac85ba562f6ab6d54d2fe6e5ec8ecfc5
yohamta <yohamta@gmail.com>
2022-10-24 01:07:58 +0900

    update RemoveChildren

--- plugins/hierarchy/hierarchy.go
+++ plugins/hierarchy/hierarchy.go
@@ -14,7 +14,8 @@ var HierarchySystem = &parent{
 
 // RemoveChildren removes children of invalid parents.
 // This function is useful when you want to remove children of invalid parents.
-// You don't need to use this system function when you use hierarchy.RemoveChildrenRecursive() function.
+// You don't need to use this system function when you use
+// hierarchy.RemoveRecursive() or hierarchy.RemoveChildrenRecursive() instead.
 func (ps *parent) RemoveChildren(ecs *ecs.ECS) {
 	ps.query.EachEntity(ecs.World, func(entry *donburi.Entry) {
 		if !entry.Valid() {
@@ -27,7 +28,7 @@ func (ps *parent) RemoveChildren(ecs *ecs.ECS) {
 			c, ok := GetChildren(entry)
 			if ok {
 				for _, e := range c {
-					defer ecs.World.Remove(e)
+					ecs.World.Remove(e)
 				}
 			}
 			entry.Remove()
