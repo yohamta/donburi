b01de8fbe8b467f08f81dad35e0cb29a3b1235e4
yohamta <yohamta@gmail.com>
2023-02-22 13:11:53 +0900

    rollback experimental feature

--- examples/platformer/archetypes/archetypes.go
+++ examples/platformer/archetypes/archetypes.go
@@ -1,35 +1,55 @@
 package archetypes
 
 import (
+	"github.com/yohamta/donburi"
 	"github.com/yohamta/donburi/ecs"
 	"github.com/yohamta/donburi/examples/platformer/components"
+	"github.com/yohamta/donburi/examples/platformer/layers"
 	"github.com/yohamta/donburi/examples/platformer/tags"
 )
 
 var (
-	Platform = ecs.NewArchetype(
+	Platform = newArchetype(
 		tags.Platform,
 		components.Object,
 	)
-	FloatingPlatform = ecs.NewArchetype(
+	FloatingPlatform = newArchetype(
 		tags.FloatingPlatform,
 		components.Object,
 		components.Tween,
 	)
-	Player = ecs.NewArchetype(
+	Player = newArchetype(
 		tags.Player,
 		components.Player,
 		components.Object,
 	)
-	Ramp = ecs.NewArchetype(
+	Ramp = newArchetype(
 		tags.Ramp,
 		components.Object,
 	)
-	Space = ecs.NewArchetype(
+	Space = newArchetype(
 		components.Space,
 	)
-	Wall = ecs.NewArchetype(
+	Wall = newArchetype(
 		tags.Wall,
 		components.Object,
 	)
 )
+
+type archetype struct {
+	components []donburi.IComponentType
+}
+
+func newArchetype(cs ...donburi.IComponentType) *archetype {
+	return &archetype{
+		components: cs,
+	}
+}
+
+func (a *archetype) Spawn(ecs *ecs.ECS, cs ...donburi.IComponentType) *donburi.Entry {
+	e := ecs.World.Entry(ecs.Create(
+		layers.Default,
+		append(a.components, cs...)...,
+	))
+	return e
+}
