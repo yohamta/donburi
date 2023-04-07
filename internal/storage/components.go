7944f2c3df930af82e9d493d935fe2f5a60a2766
Yota Hamada <yohamta@gmail.com>
2023-04-07 15:21:14 +0900

    refactor components struct to support larger number of component types

--- internal/storage/components.go
+++ internal/storage/components.go
@@ -2,31 +2,34 @@ package storage
 
 import "github.com/yohamta/donburi/component"
 
-// ComponentIndex represents the index of component in a archetype.
+// ComponentIndex represents the index of component in an archetype.
 type ComponentIndex int
 
 // Components is a structure that stores data of components.
 type Components struct {
-	storages []*Storage
-	// TODO: optimize to use slice instead of map for performance
+	storages         []*Storage
 	componentIndices map[ArchetypeIndex]ComponentIndex
 }
 
 // NewComponents creates a new empty structure that stores data of components.
 func NewComponents() *Components {
 	return &Components{
-		storages:         make([]*Storage, 512), // TODO: expand as the number of component types increases
+		storages:         make([]*Storage, 0, 512),
 		componentIndices: make(map[ArchetypeIndex]ComponentIndex),
 	}
 }
 
-// PUshComponent stores the new data of the component in the archetype.
+// PushComponents stores the new data of the component in the archetype.
 func (cs *Components) PushComponents(components []component.IComponentType, archetypeIndex ArchetypeIndex) ComponentIndex {
 	for _, componentType := range components {
-		if v := cs.storages[componentType.Id()]; v == nil {
-			cs.storages[componentType.Id()] = NewStorage()
+		id := int(componentType.Id())
+		if id >= len(cs.storages) {
+			cs.storages = append(cs.storages, make([]*Storage, id-len(cs.storages)+1)...)
 		}
-		cs.storages[componentType.Id()].PushComponent(componentType, archetypeIndex)
+		if cs.storages[id] == nil {
+			cs.storages[id] = NewStorage()
+		}
+		cs.storages[id].PushComponent(componentType, archetypeIndex)
 	}
 	if _, ok := cs.componentIndices[archetypeIndex]; !ok {
 		cs.componentIndices[archetypeIndex] = 0
@@ -44,11 +47,15 @@ func (cs *Components) Move(src ArchetypeIndex, dst ArchetypeIndex) {
 
 // Storage returns the pointer to data of the component in the archetype.
 func (cs *Components) Storage(c component.IComponentType) *Storage {
-	if storage := cs.storages[c.Id()]; storage != nil {
-		return storage
+	id := int(c.Id())
+	if id < len(cs.storages) && cs.storages[id] != nil {
+		return cs.storages[id]
 	}
-	cs.storages[c.Id()] = NewStorage()
-	return cs.storages[c.Id()]
+	if id >= len(cs.storages) {
+		cs.storages = append(cs.storages, make([]*Storage, id-len(cs.storages)+1)...)
+	}
+	cs.storages[id] = NewStorage()
+	return cs.storages[id]
 }
 
 // Remove removes the component from the storage.
