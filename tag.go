2befc7ec13617e998eed3e0022f2ba7dd1ade4fe
imthatgin <imthatgin@proton.me>
2024-05-29 15:28:25 +0200

    Fix test

--- tag.go
+++ tag.go
@@ -7,11 +7,11 @@ type Tag string
 // Specify a string as the first and only parameter if you wish to name the component.
 func NewTag(opts ...any) *ComponentType[Tag] {
 	if len(opts) == 0 {
-		return NewComponentType[Tag](Tag(""))
+		return NewComponentType[Tag]()
 	}
 	first, ok := opts[0].(string)
 	if !ok {
-		return NewComponentType[Tag](Tag(""))
+		return NewComponentType[Tag]()
 	}
 	c := NewComponentType[Tag](Tag(first))
 	c.SetName(first)
