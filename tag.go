9c88d8f5b5e40d3cc6e9b7451534a21219b264c4
imthatgin <imthatgin@proton.me>
2024-05-29 15:25:17 +0200

    Update tag.go

--- tag.go
+++ tag.go
@@ -4,8 +4,16 @@ type Tag string
 
 // NewTag is an utility to create a tag component.
 // Which is just an component that contains no data.
-func NewTag(name string) *ComponentType[Tag] {
-	c := NewComponentType[Tag](Tag(name))
-	c.SetName(name)
+// Specify a string as the first and only parameter if you wish to name the component.
+func NewTag(opts ...any) *ComponentType[Tag] {
+	if len(opts) == 0 {
+		return NewComponentType[Tag](Tag(""))
+	}
+	first, ok := opts[0].(string)
+	if !ok {
+		return NewComponentType[Tag](Tag(""))
+	}
+	c := NewComponentType[Tag](Tag(first))
+	c.SetName(first)
 	return c
 }
